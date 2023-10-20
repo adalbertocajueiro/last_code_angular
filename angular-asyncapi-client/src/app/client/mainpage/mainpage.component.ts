b18dedeef7a451b71d627cde68d7a900101f7612
Adalberto Cajueiro <adalberto.cajueiro@gmail.com>
2023-10-20 14:42:37 +0000

    working selection of topic and send payload

--- angular-asyncapi-client/src/app/client/mainpage/mainpage.component.ts
+++ angular-asyncapi-client/src/app/client/mainpage/mainpage.component.ts
@@ -1,28 +1,34 @@
-import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import { ALL_TOPICS } from '../services/topics';
 import { ClientImplementationService } from '../implementation/client_implementation';
+import { MetaInfoObject } from '../models';
 
 @Component({
   selector: 'app-mainpage',
   templateUrl: './mainpage.component.html',
-  styleUrls: ['./mainpage.component.css']
+  styleUrls: ['./mainpage.component.scss']
 })
 
-export class MainpageComponent implements OnInit,OnChanges{
+export class MainpageComponent implements OnInit,OnChanges {
   
   connectedToBroker:boolean = false
   allTopics = ALL_TOPICS
   selectedTopic?:string
+  selectedTopicPayload:any
+
+  @ViewChild("topics") topics?: ElementRef
   
   constructor(private clientImplementationService: ClientImplementationService){
-
+    
   }
+
   ngOnChanges(changes: SimpleChanges): void {
     this.isConnectedToBroker();
   }
 
   ngOnInit(): void {
-    this.selectedTopic = this.allTopics[0]
+    this.selectedTopic = this.allTopics[0][0]
+    this.changeSelectedTopic()
     this.isConnectedToBroker()
   }
 
@@ -30,13 +36,22 @@ export class MainpageComponent implements OnInit,OnChanges{
     this.connectedToBroker  = this.clientImplementationService.areServicesConnectedToBroker()
   }
 
-  changeSelectedTopic(event:any){
-    var options: HTMLOptionsCollection = event.target.options
-    const index = options.selectedIndex
-    if(index != -1){
-      this.selectedTopic = options.item(index)?.value
-      console.log(this.selectedTopic)
+  changeSelectedTopic(){
+    const selectedElement =
+      this.allTopics.find((ele: any[]) => ele[0] === this.selectedTopic)
+    if (selectedElement) {
+      var newObj = Reflect.construct(selectedElement[1], [])
+      this.selectedTopicPayload = this.buildObject(Object.keys(newObj as any))
     }
-    
+  }
+
+  buildObject(keys:string[]){
+    const result:any = {}
+    keys.map(n=>n.substring(1)).forEach(k => result[k] = 'undefined')
+    return result
+  }
+
+  convertClassStructureToJson(classStructure:any){
+    return Object.keys(classStructure)
   }
 }
--- angular-asyncapi-client/src/app/client/services/topics.ts
+++ angular-asyncapi-client/src/app/client/services/topics.ts
@@ -1,9 +1,11 @@
+import { CommandObject, MetaInfoObject, MovedObject } from "../models";
+
 export const METAINFO_TOPIC = 'metainfo';
 export const ROBOT_NAME_COMMANDS_TOPIC = 'ROBOT_NAME_commands';
 export const ROBOT_NAME_MOVED_TOPIC = 'ROBOT_NAME_moved';
 
-export const ALL_TOPICS = [
-  METAINFO_TOPIC,
-  ROBOT_NAME_COMMANDS_TOPIC,
-  ROBOT_NAME_MOVED_TOPIC,
+export const ALL_TOPICS:any[][] = [
+  [METAINFO_TOPIC,MetaInfoObject],
+  [ROBOT_NAME_COMMANDS_TOPIC,CommandObject],
+  [ROBOT_NAME_MOVED_TOPIC, MovedObject]
 ];
